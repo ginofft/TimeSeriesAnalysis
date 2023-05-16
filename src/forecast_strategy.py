387895d2d7df606e74919c63df02ef46569948b6
ginofft <ntnghia.andrei@gmail.com>
2023-05-16 16:25:27 +0700

    small refactor

--- src/forecast_strategy.py
+++ src/forecast_strategy.py
@@ -5,6 +5,7 @@ from .utils import load_checkpoint, save_checkpoint, EarlyStopper
 from .dataset import TimeSeriesDataset
 from .scaler import Scaler
 
+from pathlib import Path
 import pandas as pd
 import numpy as np
 from pmdarima import auto_arima
@@ -17,10 +18,10 @@ class ForecastStrategy(ABC):
     def load_data(self, dataframe):
         pass
     @abstractmethod
-    def train(self, input_field, output_field, h, **kwargs):
+    def train(self, input_field, output_field, forecastWindow, **kwargs):
         pass
     @abstractmethod
-    def forecast(self, input_field, oput_field, h):
+    def forecast(self, input_field, oput_field, forecastWindow):
         pass
 
 class DeepLearningStrategy(ForecastStrategy):
@@ -31,12 +32,19 @@ class DeepLearningStrategy(ForecastStrategy):
         self.arch_config_path = arch_config_path
         
     def load_data(self, dataframe) -> None:
+        self._data = dataframe
+
+    def train(self, 
+              input_field, 
+              outout_field,
+              forecastWindow,
+              preprocessPipePath: Path, 
+              scalePipePath: Path,
+              datetime_col,
+              freq) -> None:
         pass
 
-    def train(self) -> None:
-        pass
-
-    def forecast(self, input_field, output_field, h):
+    def forecast(self, input_field, output_field, forecastWindow):
         pass
 
 class SARIMAStrategy(ForecastStrategy):
@@ -45,13 +53,13 @@ class SARIMAStrategy(ForecastStrategy):
     def load_data(self, dataframe):
         self._data = dataframe
 
-    def train(self, input_field, output_field, h, m=12):
+    def train(self, input_field, output_field, forecastWindow, m=12):
         self._model = auto_arima(self._data[output_field], seasonal=True, m=m)
         self._model.fit(self._data[output_field])
         return self._model.summary()
     
-    def forecast(self, input_field, output_field, h):
-        preds = self._model.predict(n_periods=h)
+    def forecast(self, input_field, output_field, forecastWindow):
+        preds = self._model.predict(n_periods=forecastWindow)
         nan_df = pd.DataFrame(np.nan, index=preds.index, columns=self._data.columns)
         self._data = pd.concat([self._data, nan_df])
         self._data['pred'] = preds
